350ad98c7f509cd3499391a70628f6f8be1eb4c5
Mohamed Lemine tedjih <59097385+Med-lemin@users.noreply.github.com>
2025-06-23 23:29:16 +0000

    test

--- src/front/pages/Favorites.jsx
+++ src/front/pages/Favorites.jsx
@@ -5,21 +5,21 @@ const dogData = [
         id: 1,
         name: "charlie",
         breed: "some breed",
-        location:"some location",
-        image:"some url"
+        location: "some location",
+        image: "some url"
     }
 ]
 
-export default Favorites =()=>{
+export default function Favorites() {
 
-    return(
+    return (
         <div class="card" style="width: 18rem;">
-  <img src={dogData.image} class="card-img-top" alt="..."/>
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+            <img src={dogData.image} class="card-img-top" alt="..." />
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
     )
 }
