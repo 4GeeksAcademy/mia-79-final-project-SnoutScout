db700ecdc4531e266950abf4e982ae89aac772f9
Andres Jimenez <ajthecoder1@gmail.com>
2025-06-28 00:17:56 +0000

    Final project

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -1,17 +1,26 @@
 import { Link } from "react-router-dom";
+import React from "react";
 
 export const Navbar = () => {
 
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+		<nav className="navbar navbar-expand navbar-light py-2" style={{ backgroundColor: "#fd7e14" }}>
+			<div className="container-fluid">
+				<h1 className="navbar-brand fs-1 fw-bold">SnoutScout</h1>
+				<div className="d-flex flex-grow-1 justify-content-center">
+					<div className="d-none d-md-flex gap-4">
+						<a href="#" className="nav-link text-white">Home</a>
+						<a href="#" className="nav-link text-white">Messages</a>
+						<a href="#" className="nav-link text-white">Browse</a>
+						<a href="#" className="nav-link text-white">Favorites</a>
+					</div>
+				</div>
+
+				<div className="d-flex w-100 justify-content-end">
+					<div className="col-auto">
+						<button className="btn btn-light" style={{ color: "#fd7e14" }} type="submit">Join Now</button>
+					</div>
+
 				</div>
 			</div>
 		</nav>
