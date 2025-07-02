fde992c455bbf717d42e9fb55902e1dfa71ad124
Ian Garvey <ianarthurgarvey@gmail.com>
2025-07-02 23:14:43 +0000

    build the backend for the messages page

--- src/api/models.py
+++ src/api/models.py
@@ -1,11 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, Boolean  
 from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[int] = mapped_column(String(50), nullable=True) # for sign up form, make sure to include input for name
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
@@ -16,4 +18,21 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+class Message(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    message_from: Mapped[int] = mapped_column(nullable=False, unique=True)
+    message_to: Mapped[int] = mapped_column(nullable=False, unique=True)
+    content: Mapped[str] = mapped_column(nullable=False)
+    created_at: Mapped[datetime] = mapped_column(default=lambda: datetime.now)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "message_from": self.message_from,
+            "message_to": self.message_to,
+            "favorite_dog_id": self.favorited_dog_id,
+            "content": self.content,
+            "created_at": self.created_at,
         }
