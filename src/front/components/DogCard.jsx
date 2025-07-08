d72d06d26d836f21c37b5f61aeafd7a51b0101d9
Ernesto Medina <ernesto.m.medina.m@gmail.com>
2025-07-08 01:24:35 +0000

    :construction: Works on DogCarousel & petfinder fetch

--- src/front/components/DogCard.jsx
+++ src/front/components/DogCard.jsx
@@ -1,57 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Card, Badge, Button, Modal, ListGroup } from 'react-bootstrap';
-import { useReducer } from 'react';
-import useGlobalReducer from '../hooks/useGlobalReducer';
 
 export default function DogCard({ dog }) {
-  // const [state, dispatch] = useReducer(stateReducer, initialStore)
-  //  const [token, setToken] = useState (null);
-  const {store, dispatch} = useGlobalReducer ()
-
+  
   const [showModal, setShowModal] = useState(false);
 
-  const PetAuth= async() => {
-    const result= await fetch ("https://turbo-space-train-x5pjwrx79p6726x7g-3001.app.github.dev/api/pets/",{
-      method: "GET", 
-      headers: {
-        "Content-Type": "application/json",
-      },})
-      const body = await result.json()
-      dispatch ({
-        type: "set_pets",
-        payload: body
-      })
-    }
 
-  //     body: JSON.stringify({      
-  //       grant_type: "client_credentials",      
-  //       client_id: import.meta.env.VITE_PET_FINDER_CLIENT_ID,      
-  //       client_secret: import.meta.env.VITE_PET_FINDER_SECRET,
-  //     })
-  //   })
-  //   const data = await result.json ()
-  //   setToken (data.accessToken)
-  //   return data
-  // }
-
-  // const getDogs= async (token) => {
-  //   const result= await fetch ("https://api.petfinder.com/v2/animals",{
-  //     method: "GET", 
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //       "Authorization": `Bearer ${token}`
-  //     }, 
-  //   })
-  //   const data = await result.json ()
-  //   return data
-  // }
-
-  useEffect(() => {
-    const getTokenAndFetchPets = async () => {
-           const authData = await PetAuth(); 
-        };  
-        getTokenAndFetchPets();
-  }, []);
 
   return (
     <>
@@ -59,7 +13,7 @@ export default function DogCard({ dog }) {
       <Card style={{ width: '22rem' }} className="mx-4 text-center">
         <Card.Img
           variant="top"
-          src={dog.imgUrl}
+          src={dog.photos[0].large}
           alt={dog.name}
           style={{ cursor: 'pointer' }}
           onClick={() => setShowModal(true)}
@@ -90,13 +44,13 @@ export default function DogCard({ dog }) {
         </Modal.Header>
         <Modal.Body>
           <Card.Img
-            src={dog.imgUrl}
+            src={dog.photos[0].small}
             alt={dog.name}
             className="mb-3 rounded"
           />
           <ListGroup variant="flush">
             <ListGroup.Item><strong>Age:</strong> {dog.age} years</ListGroup.Item>
-            <ListGroup.Item><strong>Location:</strong> {dog.location}</ListGroup.Item>
+            <ListGroup.Item><strong>Location:</strong> {dog.contact?.address?.city}</ListGroup.Item>
             {dog.tags.map(tag => (
               <ListGroup.Item key={tag}>
                 <strong>Tag:</strong> {tag}
--- src/front/components/DogCarousel.jsx
+++ src/front/components/DogCarousel.jsx
@@ -1,49 +1,71 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Button } from 'react-bootstrap';
 import DogCard from './DogCard';
+import useGlobalReducer from '../hooks/useGlobalReducer';
 
-const dogs = [
-  {
-    id: 1,
-    name: "Jack",
-    age: 2,
-    location: "Orlando, Florida",
-    tags: ["Loyal", "Active", "Friendly"],
-    imgUrl: "https://media.4-paws.org/0/c/7/e/0c7e157aabf6ef2f316c05607813b130c994f293/VIER%20PFOTEN_2019-03-15_001-2886x1999.jpg"
-  },
-  {
-    id: 2,
-    name: "Charlie",
-    age: 2,
-    location: "Austin, Texas",
-    tags: ["Playful", "Calm"],
-    imgUrl: "https://www.petlandtexas.com/wp-content/uploads/2022/04/shutterstock_1290320698-1-scaled.jpg"
-  },
-  {
-    id: 3,
-    name: "Max",
-    age: 3,
-    location: "Denver, Colorado",
-    tags: ["Active", "Protective", "Family-Oriented"],
-    imgUrl: "https://cdn.royalcanin-weshare-online.io/3DKT5m8BN5A8uWWASDMR/v1/ptpc1s3-welsh-pembroke-corgi-puppy-running-outside-in-a-garden"
-  },
-];
+// const dogs = [
+//   {
+//     id: 1,
+//     name: "Jack",
+//     age: 2,
+//     location: "Orlando, Florida",
+//     tags: ["Loyal", "Active", "Friendly"],
+//     imgUrl: "https://media.4-paws.org/0/c/7/e/0c7e157aabf6ef2f316c05607813b130c994f293/VIER%20PFOTEN_2019-03-15_001-2886x1999.jpg"
+//   },
+//   {
+//     id: 2,
+//     name: "Charlie",
+//     age: 2,
+//     location: "Austin, Texas",
+//     tags: ["Playful", "Calm"],
+//     imgUrl: "https://www.petlandtexas.com/wp-content/uploads/2022/04/shutterstock_1290320698-1-scaled.jpg"
+//   },
+//   {
+//     id: 3,
+//     name: "Max",
+//     age: 3,
+//     location: "Denver, Colorado",
+//     tags: ["Active", "Protective", "Family-Oriented"],
+//     imgUrl: "https://cdn.royalcanin-weshare-online.io/3DKT5m8BN5A8uWWASDMR/v1/ptpc1s3-welsh-pembroke-corgi-puppy-running-outside-in-a-garden"
+//   },
+// ];
 
 export default function DogCarousel() {
   const [current, setCurrent] = useState(0);
+  const { store, dispatch } = useGlobalReducer();
+  const PetAuth = async () => {
+    const url = `${import.meta.env.VITE_BACKEND_URL}/api/pets`;
+    const result = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    let body = await result.json();
+    body = body.filter((pet) => !!pet.photos && pet.photos.length > 1)
+    dispatch({
+      type: "set_pets",
+      payload: body
+    });
+  };
 
   const prev = () =>
-    setCurrent(i => (i - 1 + dogs.length) % dogs.length);
+    setCurrent(i => (i - 1 + store.pets.length) % store.pets.length);
   const next = () =>
-    setCurrent(i => (i + 1) % dogs.length);
+    setCurrent(i => (i + 1) % store.pets.length);
 
+  useEffect(() => {
+    const getTokenAndFetchPets = async () => await PetAuth();
+    getTokenAndFetchPets();
+  }, []);
   return (
     <Container className="d-flex justify-content-center align-items-center my-5">
       <Button variant="outline-secondary" onClick={prev} aria-label="Previous">
         ‹
       </Button>
-
-      <DogCard dog={dogs[current]} />
+      {!!store.pets && store.pets.length > 1 && (
+        <DogCard dog={store.pets[current]} />
+      )}
 
       <Button variant="outline-secondary" onClick={next} aria-label="Next" className="ms-3">
         ›
